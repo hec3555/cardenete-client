7008d6ad864b937ffcd8695b6e3256fc2f65cdbe
unknown <hec3555@gmail.com>
2019-06-12 05:08:22 +0200

    Confirmar  cuenta desde mail funcionando

--- src/app/auth-components/confirmar-cuenta/confirmar-cuenta.component.ts
+++ src/app/auth-components/confirmar-cuenta/confirmar-cuenta.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { LoginService } from 'src/app/service/login/login.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-confirmar-cuenta',
   templateUrl: './confirmar-cuenta.component.html',
-  styleUrls: ['./confirmar-cuenta.component.css']
+  styleUrls: ['./confirmar-cuenta.component.css'],
+  providers: [MessageService]
 })
 export class ConfirmarCuentaComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private messageService: MessageService,
+    private loginService: LoginService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
 
   ngOnInit() {
+    this.route.params.subscribe(data => {
+      this.loginService.confirmUsuario(data.login, data.token).subscribe(
+        data => this.usuarioConfirmed(data.msg)
+      )
+    })
+  }
+
+
+  usuarioConfirmed(msg: String){
+    this.showTooltip("success", "Confirmado", msg)
+    setTimeout(() => {
+      this.router.navigate(['/login']);
+    }, 2000);
+  }
+
+
+
+  showTooltip(type: string, title: string, desc: String) {
+    this.messageService.add({
+      severity: `${type}`,
+      summary: `${title}`,
+      detail: `${desc}`
+    })
   }
 
 }
--- src/app/service/login/login.service.ts
+++ src/app/service/login/login.service.ts
@@ -26,4 +26,18 @@ export class LoginService {
         this.usuario = null;
         this.http.get(`${this.configAPI.api}logout`, this.configAPI.header).subscribe();
     }
+
+    registrar(usuario: UsuarioInterface): Observable<ResponseInterface> {
+        return this.http.post<ResponseInterface>(`${this.configAPI.api}registrar`, usuario, this.configAPI.header);
+    }
+
+    confirmUsuario(login: String, token: String): Observable<ResponseInterface> {
+        return this.http.get<ResponseInterface>(`${this.configAPI.api}confirm/${login}/${token}`, this.configAPI.header);
+    }
+    checkUsernameAvailability(login: String): Observable<ResponseInterface> {
+        return this.http.get<ResponseInterface>(`${this.configAPI.api}check-username-availability/${login}`, this.configAPI.header);
+    }
+    checkEmailAvailability(email: String): Observable<ResponseInterface> {
+        return this.http.get<ResponseInterface>(`${this.configAPI.api}check-email-availability/${email}`, this.configAPI.header);
+    }
 }
